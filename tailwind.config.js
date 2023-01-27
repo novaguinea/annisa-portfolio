99211f5c793a9a279820fa99994efa3287ec26a2
novaguinea <novaguinea.a@gmail.com>
2023-01-27 13:06:43 +0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  darkMode: 'class',
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx}",
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+ 
+    // Or if using `src` directory:
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: "Inter",
+      boxShadow: {
+        'custom': '0 16px 50px -20px #fecdd3',
+      }
+    },
+  },
+  plugins: [],
+}
